d42a69fb98cc284539ac6afdd6c22f962d00ef2a
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-06-01 20:57:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+import time
+import winsound
+
+
+class manager:
+    def __init__(self):
+        self.store = ""
+        self.model = ""
+        self.serial_number = ""
+
+    def add(self):
+        pass
+
+    def update(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_list(self):
+        pass
+
+    def terminate(self):
+        pass
+
+    def get_list(self):
+        pass

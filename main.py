f30ffd8aec8a408eb28f9cf8aeca031c0417de80
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-06-15 21:53:02 +0100

    Run time error to get products

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ PRODUCTS['987654321'] = {
 def show_products():
     for item in PRODUCTS:
         get_product(item)
-        print('-----------')
+        print('-------------------------------------------------------')
 
 
 def get_product(item):
-    print('Serial Number: ', item)
-    print('Model: ', PRODUCTS[item]['Model'])
-    print('Year: ', PRODUCTS[item]['Year'])
-    print('Store: ', PRODUCTS[item]['Store'])
+    try:
+        print('Serial Number: ', item)
+        print('Model: ', PRODUCTS[item]['Model'])
+        print('Year: ', PRODUCTS[item]['Year'])
+        print('Store: ', PRODUCTS[item]['Store'])
+    except KeyError:
+        print('>>>> Item does not found')
+    except Exception as error:
+        print('>>>> Sorry, we had a problem and we are work to fix it')
+        print(error)
 
 
 def item_details():
@@ -42,6 +48,19 @@ def add_product(item, Model, Year, Store):
     print('>>>The Serial Number:{} was add'.format(item))
 
 
+def delete_item(item):
+    try:
+        PRODUCTS.pop(item)
+        print()
+        print('>>>> The Serial Number: {} was deleted'.format(item))
+        print()
+    except KeyError:
+        print('>>>> Serial Number does not exist')
+    except Exception as error:
+        print('>>>> Sorry, we had a problem and we are work to fix it')
+        print(error)
+
+
 def show_menu():
     print('-------------------------------------------------------')
     print('1 - Show all Products')
@@ -53,32 +72,39 @@ def show_menu():
     print('-------------------------------------------------------')
 
 
-show_menu()
+while True:
+    show_menu()
 
-option = input('CHOSE A OPTION: ')
+    option = input('CHOSE A OPTION: ')
 
+    if option == '1':
+        show_products()
+    elif option == '2':
+        item = input('Type The Serial Number: ')
+        get_product(item)
+    elif option == '3':
+        item = input('Type the Serial Number: ')
 
-if option == '1':
-    show_products()
-elif option == '2':
-    item = input('Type The Serial Number: ')
-    get_product(item)
-elif option == '3':
-    item = input('Type the Serial Number: ')
+        try:
+            PRODUCTS[item]
+            print('>>>> Product already Registed')
+        except KeyError:
+            model, year, store = item_details()
+            add_product(item, model, year, store)
+    elif option == '4':
+        item = input('Type the Serial Number: ')
 
-    try:
-        PRODUCTS[item]
-        print('>>>> Product already Registed')
-    except KeyError:
-        model, year, store = item_details()
-        add_product(item, model, year, store)
-elif option == '4':
-    item = input('Type the Serial Number: ')
-
-    try:
-        PRODUCTS[item]
-        print('>>>> updating Item:', item)
-        model, year, store = item_details()
-        add_product(item, model, year, store)
-    except KeyError:
-        print('>>>> Product already Registed')
+        try:
+            PRODUCTS[item]
+            print('>>>> updating Item:', item)
+            model, year, store = item_details()
+            add_product(item, model, year, store)
+        except KeyError:
+            print('>>>> Product already Registed')
+    elif option == '5':
+        item = input('Type the Serial Number: ')
+        delete_item(item)
+    elif option == '0':
+        print('')
+    else:
+        print('Type a Valide Number')
